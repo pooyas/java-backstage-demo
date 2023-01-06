41e8321afef7a24d7c11b47713f985faf1498c6a
Pooya Saeedi <psaeedi@coursera.org>
2023-01-05 21:10:36 -0500

    First commit

---
+++ src/main/java/com/example/javabackstagedemo/JavaBackstageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabackstagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackstageDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackstageDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javabackstagedemo/JavaBackstageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabackstagedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackstageDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
